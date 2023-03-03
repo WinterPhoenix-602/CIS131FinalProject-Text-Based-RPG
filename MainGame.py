74b65b78d3a25241ca5f7ea146ac991c03e8ba75
WinterPhoenix-602 <clwilson602@gmail.com>
2023-03-02 20:27:31 -0700

    Removed testing from main game file

--- MainGame.py
+++ MainGame.py
@@ -4,15 +4,8 @@
 #Final Project: Text Based RPG
 
 from PlayerClass import Player
-from EnemyClass import Enemy
 
 def main():
-    player = Player('Player', 100, 100, 5, 1)
-    goblin = Enemy('Goblin', 25, 0, 2, 1)
-    player.printStats()
-    goblin.printStats()
-    player.modifyHealth(goblin.damage)
-    player.modifyMagic(-10)
-    player.printStats()
+    pass
 
 main()
