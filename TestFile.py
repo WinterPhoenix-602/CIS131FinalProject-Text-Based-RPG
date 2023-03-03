9e0e9be67b722bcb24cf3c0061df844432707b7a
WinterPhoenix-602 <clwilson602@gmail.com>
2023-03-02 20:27:43 -0700

    Started testing Location tiles and reading informaion from json

--- TestFile.py
+++ TestFile.py
@@ -3,8 +3,19 @@
 #CIS131
 #Final Project: Testing
 
+import json
 from PlayerClass import Player
 from EnemyClass import Enemy
+from LocationTileClass import Tile
+
+with open("Tiles.json",'r') as file:
+  a=file.readlines()
+dict_0=json.loads(a[0])
+
+tile_instance = Tile()
+tile_instance.reader(dict_0)
+
+print(f'{tile_instance.name} {tile_instance.enemy.name}')
 
 def main():
     player = Player('Player', 100, 100, 5, 1)
@@ -15,4 +26,4 @@ def main():
     player.modifyMagic(-10)
     player.printStats()
 
-main()
+# main()
