29e97c17d2ab2f64e8b933317eb85c520466c019
WinterPhoenix-602 <clwilson602@gmail.com>
2023-03-06 22:51:29 -0700

    moved menu() function below main()

--- TestFile.py
+++ TestFile.py
@@ -8,33 +8,6 @@ from PlayerClass import Player
 from EnemyClass import Enemy
 from LocationTileClass import Tile
 
-def menu(tileCoords, tiles_dict):
-    while True:
-        try:
-            choice = int(input('What would you like to do?\n1: Go North\n2: Go East\n3: Go South\n4: Go West\n5: Open Inventory\n6: Exit Game\n? '))
-            match choice:
-                case 1:
-                    tileCoords[1] += 1
-                    break
-                case 2:
-                    tileCoords[0] += 1
-                    break
-                case 3: 
-                    tileCoords[1] -= 1
-                    break
-                case 4:
-                    tileCoords[0] -= 1
-                    break
-                case 5:
-                    print('Yet to be implemented.')
-                    continue
-                case 6:
-                    break
-        except TypeError:
-            print('I\'m sorry, that\'s not a valid choice.')
-            continue
-    return tileCoords, tiles_dict, choice
-
 def main():
     tileCoords = [0,0]
     currentTile = Tile()
@@ -67,16 +40,50 @@ def main():
                 case 4:
                     tileCoords[0] += 1
 
+def menu(tileCoords, tiles_dict):
+    while True:
+        try:
+            choice = int(input('What would you like to do?\n1: Go North\n2: Go East\n3: Go South\n4: Go West\n5: Open Inventory\n6: Exit Game\n? '))
+            match choice:
+                case 1:
+                    tileCoords[1] += 1
+                    break
+                case 2:
+                    tileCoords[0] += 1
+                    break
+                case 3: 
+                    tileCoords[1] -= 1
+                    break
+                case 4:
+                    tileCoords[0] -= 1
+                    break
+                case 5:
+                    print('Yet to be implemented.')
+                    continue
+                case 6:
+                    break
+        except TypeError:
+            print('I\'m sorry, that\'s not a valid choice.')
+            continue
+    return tileCoords, tiles_dict, choice
+
 def combat(currentTile):
     while len(currentTile.enemies_dict) > 0:
+        #removes dead enemies from enemies dictionary
         for i in list(currentTile.enemies_dict.keys()):
             if currentTile.enemies_dict[i].health <= 0:
                 del currentTile.enemies_dict[i]
+        
+        #prints list of enemies and relevant stats
         print(f'Name\t\tHealth\tDamage\t')
         print(f'__________________________________________________')
         for enemy in currentTile.enemies_dict:
             currentTile.enemies_dict[enemy].print_stats()
         print(f'__________________________________________________')
+
+        #allows players to use actions
+        choice = int(input('What would you like to do?\n1: Attack\n2:? '))
+
         break
 
 main()
