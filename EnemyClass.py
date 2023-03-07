8247779a335530a37d57b64342b81fe1af2bc72a
WinterPhoenix-602 <clwilson602@gmail.com>
2023-03-06 23:05:02 -0700

    removed modify_magic() method

--- EnemyClass.py
+++ EnemyClass.py
@@ -11,9 +11,6 @@ class Enemy:
 
     def modify_health(self, change):
         self.health += change
-
-    def modify_magic(self, change):
-        self.magic += change
     
     def print_stats(self):
         print(f'{self.name}\t{self.health}\t{self.damage}')
