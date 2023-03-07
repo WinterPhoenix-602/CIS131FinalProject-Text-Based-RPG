b615a4934bf3d4dd512ac6b725619af24723497c
WinterPhoenix-602 <clwilson602@gmail.com>
2023-03-07 13:14:02 -0700

    fixed error in list_items method

--- InventoryClass.py
+++ InventoryClass.py
@@ -10,5 +10,5 @@ class Inventory:
 
     def list_items(self):
         for item in self.items:
-            if self.items[item > 0]:
+            if self.items[item] > 0:
                 print(f"Name: {item} Quantity: {self.items[item]}")
