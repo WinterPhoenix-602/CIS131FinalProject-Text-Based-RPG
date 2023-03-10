a0ae078b6184642720ec04a4b8b0453da75e71b8
WinterPhoenix-602 <clwilson602@gmail.com>
2023-03-10 15:48:02 -0700

    Inventory display overhaul changed list_stats back to __str__, now returns formatted table split attack method to two seperate melee_attack/cast_fireball methods added equippedWeapon and equippedShield parameters simplified several places by using new parameters

--- PlayerClass.py
+++ PlayerClass.py
@@ -4,10 +4,11 @@
 #Final Project: Player Class
 
 from ItemClass import Item
+from tabulate import tabulate
 
 class Player:
     #initialization method
-    def __init__(self, name = "Player", health = 100, maxHealth = 100, mana = 50, maxMana = 50, damage = 1, defense = 1, shieldDuration = 0, inventory = {"Equipped":{"Weapon":"Fists", "Shield":"None"}, "Weapon":{"Fists":{"stats":{"Damage":1}, "quantity":2}, "Wooden Sword":{"stats":{"Damage":5}, "quantity":1}}, "Shield":{"Wooden Shield":{"stats":{"Defense":2}, "quantity":1}}, "Consumable":{"Burrito":{"stats":{"Health":15}, "quantity":3}}}):
+    def __init__(self, name = "Player", health = 100, maxHealth = 100, mana = 50, maxMana = 50, damage = 1, defense = 1, shieldDuration = 0, inventory = {}, equippedWeapon = "", equippedShield = ""):
         self._name = name
         self._health = health
         self._maxHealth = maxHealth
@@ -17,6 +18,8 @@ class Player:
         self._defense = defense
         self._shieldDuration = shieldDuration
         self._inventory = inventory
+        self._equippedWeapon = equippedWeapon
+        self._equippedShield = equippedShield
 
     #getters
     def get_name(self):
@@ -37,6 +40,10 @@ class Player:
         return self._shieldDuration
     def get_inventory(self):
         return self._inventory
+    def get_equippedWeapon(self):
+        return self._equippedWeapon
+    def get_equippedShield(self):
+        return self._equippedShield
     
     #setters
     def set_name(self, name):
@@ -57,8 +64,12 @@ class Player:
         self._shieldDuration = shieldDuration
     def set_inventory(self, inventory):
         self._inventory = inventory
+    def get_equippedWeapon(self, equippedWeapon):
+        self._equippedWeapon = equippedWeapon
+    def get_equippedShield(self, equippedShield):
+        self._equippedShield = equippedShield
 
-    #sets attributes from input dictionary
+    #sets attributes from input dictionary, initializes inventory with item objects
     def reader(self, input_dict):
         for key in input_dict:
             try:
@@ -70,8 +81,8 @@ class Player:
             if itemType != "Equipped":
                 for item in self._inventory[itemType]:
                     self._inventory[itemType][item] = Item(itemType, item, self._inventory[itemType][item]["stats"], self._inventory[itemType][item]["quantity"])
-        self.equip_item(self._inventory["Weapon"][self._inventory["Equipped"]["Weapon"]])
-        self.equip_item(self._inventory["Shield"][self._inventory["Equipped"]["Shield"]])
+        self._equippedWeapon = self._inventory["Weapon"][self._inventory["Equipped"]["Weapon"]]
+        self._equippedShield = self._inventory["Shield"][self._inventory["Equipped"]["Shield"]]
     
     #adds/subtracts value to health, displays appropriate message
     def modify_health(self, change):
@@ -99,17 +110,15 @@ class Player:
             print(f"Your mana regenerates {50 - self._mana} points.\n")
             self._mana += (50 - self._mana)
 
-    #attacks input target
-    def attack(self, target, attackType):
-        match attackType:
-            case "melee":
-                target.modify_health(-(self._damage))
-                print(f"You hit {target.get_name()} and deal {self._damage} damage!")
-            case "fireball":
-                target.modify_health(-8)
-                print(f"The fire engulfs {target.get_name()} and deals 8 damage!")
-            case _:
-                print("Invalid attack type.")
+    #uses a melee attack on input target
+    def melee_attack(self, target):
+        target.modify_health(-(self._damage))
+        print(f"You hit {target.get_name()} and deal {self._damage} damage!")
+    
+    #casts fireball on input target
+    def cast_fireball(self, target):
+        target.modify_health(-8)
+        print(f"The fire engulfs {target.get_name()} and deals 8 damage!")
 
     #adds/subtracts turns to shield duration
     def modify_shieldDuration(self, turns):
@@ -122,38 +131,74 @@ class Player:
             self._defense = 2
         else:
             self._defense = 1
-
     
-    def equip_item(self, selected):
+    #equips selected item
+    def equip_item(self, selected):            
         if selected.get_itemType() == "Weapon":
-            self._inventory["Equipped"]["Weapon"] = selected.get_name()
+            if selected.get_name() != "Fists" and self._equippedWeapon.get_name() != "Fists":
+                print(f"You stow away your {self._equippedWeapon.get_name()} and equip your {selected.get_name()}.\n")
+            else:
+                print(f"You stow away your {self._equippedWeapon.get_name()}.\n")
+            self._equippedWeapon = selected
             self._damage = selected.get_stats()["Damage"]
         if selected.get_itemType() == "Shield":
-            self._inventory["Equipped"]["Shield"] = selected.get_name()
+            if selected.get_name() != "Fists" and self._equippedShield.get_name() != "Fists":
+                print(f"You stow away your {self._equippedShield} and equip your {selected.get_name()}.\n")
+            else:
+                print(f"You stow away your {self._equippedShield}.\n")
+            self._equippedShield = selected
             self._defense = selected.get_stats()["Defense"]
+    
+    #uses selected item
+    def use_item(self, item):
+        if item in self._inventory["Consumable"]:
+            if "Potion" in item:
+                print(f"You pop the cork from the vial, and down the {item} within.\n")
+            else:
+                print(f"You quickly scarf down the {item}.\n")
 
     #returns formatted inventory table representation
-    def inventory_table(self, invType):
-        inventoryTable = []
+    def get_inventory_table(self, invType):
+        equippedTable = []
+        weaponTable = []
+        shieldTable = []
+        consumableTable = []
         if invType == "Full":
             for itemType in self._inventory:
-                if itemType != "Equipped":
-                    inventoryTable.append([f"{itemType} Name", "Stats", "Amount"])
+                if itemType == "Equipped":
+                    equippedTable.append([f"{itemType} Items", "Name", "Stats"])
+                    equippedTable.append(["Weapon", self._equippedWeapon.get_name(), f"Damage: {self._equippedWeapon.get_stats()['Damage']}"])
+                    equippedTable.append(["Shield", self._equippedShield.get_name(), f"Defense: {self._equippedShield.get_stats()['Defense']}"])
+                elif itemType == "Weapon":
+                    weaponTable.append([f"{itemType} Name", "Stats"])
                     for item in self._inventory[itemType]:
-                        if item != "Fists" and item != "Unequipped":
-                            inventoryTable.append([item, f"{list(self._inventory[itemType][item].get_stats().keys())[0]}: {self._inventory[itemType][item].get_stats()[list(self._inventory[itemType][item].get_stats().keys())[0]]}", self._inventory[itemType][item].get_quantity()])
-        if invType == "Weapon":
+                        weaponTable.append([item, f"{list(self._inventory[itemType][item].get_stats().keys())[0]}: {self._inventory[itemType][item].get_stats()[list(self._inventory[itemType][item].get_stats().keys())[0]]}"])
+                elif itemType == "Shield":
+                    shieldTable.append([f"{itemType} Name", "Stats"])
+                    for item in self._inventory[itemType]:
+                        shieldTable.append([item, f"{list(self._inventory[itemType][item].get_stats().keys())[0]}: {self._inventory[itemType][item].get_stats()[list(self._inventory[itemType][item].get_stats().keys())[0]]}"])
+                elif itemType == "Consumable":
+                    consumableTable.append([f"{itemType} Name", "Stats", "Amount"])
+                    for item in self._inventory[itemType]:
+                        consumableTable.append([item, f"{list(self._inventory[itemType][item].get_stats().keys())[0]}: {self._inventory[itemType][item].get_stats()[list(self._inventory[itemType][item].get_stats().keys())[0]]}", self._inventory[itemType][item].get_quantity()])
+            return f"{tabulate(equippedTable, tablefmt='fancy_grid')}\n{tabulate(weaponTable, tablefmt='fancy_grid')}\n{tabulate(shieldTable, tablefmt='fancy_grid')}\n{tabulate(consumableTable, tablefmt='fancy_grid')}"
+        elif invType == "Weapon":
             for count, item in enumerate(self._inventory[invType]):
-                inventoryTable.append([f"{count + 1}: {item}", f"{list(self._inventory[invType][item].get_stats().keys())[0]}: {self._inventory[invType][item].get_stats()[list(self._inventory[invType][item].get_stats().keys())[0]]}", self._inventory[invType][item].get_quantity()])
-        if invType == "Shield":
+                weaponTable.append([f"{count + 1}: {item}", f"{list(self._inventory[invType][item].get_stats().keys())[0]}: {self._inventory[invType][item].get_stats()[list(self._inventory[invType][item].get_stats().keys())[0]]}"])
+            return tabulate(weaponTable, tablefmt="fancy_grid")
+        elif invType == "Shield":
             for count, item in enumerate(self._inventory[invType]):
-                inventoryTable.append([f"{count + 1}: {item}", f"{list(self._inventory[invType][item].get_stats().keys())[0]}: {self._inventory[invType][item].get_stats()[list(self._inventory[invType][item].get_stats().keys())[0]]}", self._inventory[invType][item].get_quantity()])
-        return inventoryTable
+                shieldTable.append([f"{count + 1}: {item}", f"{list(self._inventory[invType][item].get_stats().keys())[0]}: {self._inventory[invType][item].get_stats()[list(self._inventory[invType][item].get_stats().keys())[0]]}"])
+            return tabulate(shieldTable, tablefmt="fancy_grid")
+        elif invType == "Consumable":
+            for count, item in enumerate(self._inventory[invType]):
+                consumableTable.append([f"{count + 1}: {item}", f"{list(self._inventory[invType][item].get_stats().keys())[0]}: {self._inventory[invType][item].get_stats()[list(self._inventory[invType][item].get_stats().keys())[0]]}", self._inventory[invType][item].get_quantity()])
+            return tabulate(consumableTable, tablefmt="fancy_grid")
     
     #returns formatted list representation
-    def list_stats(self):
-        table = [["Name", "Health", "Mana", "Damage", "Defense"], [self._name, self._health, self._mana, f"{self._inventory['Weapon'][self._inventory['Equipped']['Weapon']].get_stats()['Damage']} ({self._inventory['Weapon'][self._inventory['Equipped']['Weapon']].get_name()})", f"{self._inventory['Shield'][self._inventory['Equipped']['Shield']].get_stats()['Defense']} ({self._inventory['Shield'][self._inventory['Equipped']['Shield']].get_name()})"]]
+    def __str__(self):
+        table = [["Name", "Health", "Mana", "Damage", "Defense"], [self._name, self._health, self._mana, f"{self._equippedWeapon.get_stats()['Damage']} ({self._equippedWeapon.get_name()})", f"{self._equippedShield.get_stats()['Defense']} ({self._equippedShield.get_name()})"]]
         if self._shieldDuration > 0:
             table[0].append("Shield")
             table[1].append(f"{self._shieldDuration} turns left")
-        return table
+        return tabulate(table, tablefmt="fancy_grid")
