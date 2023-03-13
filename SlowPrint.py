a3bc892e3213971b803037930f8e52eea87c7161
WinterPhoenix-602 <clwilson602@gmail.com>
2023-03-13 12:43:12 -0700

    Added option to change color

--- SlowPrint.py
+++ SlowPrint.py
@@ -1,13 +1,18 @@
 import time
+import termcolor
+from termcolor import colored
 from tabulate import tabulate
 
-def slowLinePrint(string="Testing, 1, 2.", delay = 0.05):
+def slowLinePrint(string="Testing, 1, 2.", delay = 0.05, color="white"):
     for character in string:
-        print(character, end="")
+        print(colored(character, color), end="")
         time.sleep(delay)
+    print("")
 
-def slowTablePrint(string=tabulate([["Testing"], [1], [2]], tablefmt="fancy_outline"), delay = 0.05):
+def slowTablePrint(string=tabulate([["Testing"], [1], [2]], tablefmt="fancy_outline"), delay = 0.05, color="white"):
     for line in string.splitlines():
-        print(line)
+        print(colored(line, color))
         time.sleep(delay)
-
+for color in termcolor.COLORS:
+    slowLinePrint(color, color=color)
+    slowTablePrint(color, color=color)
